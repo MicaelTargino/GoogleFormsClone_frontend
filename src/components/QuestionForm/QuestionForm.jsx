71f06e78a0d388f4593f3e9d8b42a096ba2e37eb
Micael Gomes Targino <micaeltargino@Luizs-MacBook-Pro.local>
2023-06-01 10:02:40 -0300

    option add and question type edit

--- src/components/QuestionForm/QuestionForm.jsx
+++ src/components/QuestionForm/QuestionForm.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect, useContext} from 'react';
-import {CropOriginal, CheckBox, ShortText, Subject, MoreVert, Add ,FilterNone, AddCircleOutline, OndemandVideo, TextFields, Close} from '@material-ui/icons';
-import {Select, Switch,Typography,IconButton,Accordion, AccordionSummary, AccordionDetails, Button, Radio, FormControlLabel, MenuItem} from '@material-ui/core';
+import {CropOriginal, CheckBox, ShortText, Subject, MoreVert, Add ,FilterNone, AddCircleOutline, OndemandVideo, TextFields, Close, PlaylistAdd} from '@material-ui/icons';
+import {Select, Switch,Typography,IconButton,Accordion, AccordionSummary, AccordionDetails, Button, Radio, FormControlLabel, MenuItem, Tooltip} from '@material-ui/core';
 import { BsTrash, BsFileText, FcRightUp } from 'react-icons';
 import DeleteIcon from '@material-ui/icons/Delete';
 import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
@@ -13,7 +13,9 @@ const QuestionForm = ({editingMode}) => {
             id: 1,
             questionText: '',
             questionType: 'text',
-            options: [],
+            options: [
+                // {optionText: 'Teste'}
+            ],
             open: true,
             required: true
         }
@@ -30,13 +32,18 @@ const QuestionForm = ({editingMode}) => {
         let newQuestions = [...questions, {
             id: uniqueIdentifier,
             questionText: '',
-            questionType: 'text',
+            questionType: '',
             options: [
                 
             ],
             open: true,
             required: true
         }];
+        setQuestions(newQuestions);
+    }
+    const addEmptyOption = (idx) => {
+        let newQuestions = [...questions];
+        newQuestions[idx].options.push({optionText: ''});
         setQuestions(newQuestions);
     }
     return (
@@ -62,10 +69,15 @@ const QuestionForm = ({editingMode}) => {
                                 <input type="text" className="question" placeholder={ques.questionText == '' ? "Type a Question": ques.questionText} onChange={(e) => {ques.questionText = e.target.value}} defaultValue={ques.questionText}></input>
                                 <CropOriginal style={{ color:'#5f6368' }} />
                                 <div>
-                                <Select className="select" defaultValue="text" onChange={e =>{ ques.questionType = e.target.value}} style={{ color:'#5f6368', fontSize: '13px', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                                <Select className="select" defaultValue={ques.questionType} onChange={e =>{
+                                     ques.questionType = e.target.value;
+                                     const questionsAfterTypeChange = [...questions];
+                                     questions[i].questionType = e.target.value;
+                                     setQuestions(questionsAfterTypeChange);
+                                     }} style={{ color:'#5f6368', fontSize: '13px', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
                                     <MenuItem id="text" value="text"><Subject style={{ marginRight: '10px' }}/> Paragraph</MenuItem>
                                     <MenuItem id="checkbox" value="checkbox"><CheckBox style={{ marginRight: '10px', color: '#70757a' }} checked></CheckBox> Checkbox</MenuItem>
-                                    <MenuItem id="radio" value="radio"><Radio style={{ margin: '0px', color: '#70757a' }} /> Multiple Choices</MenuItem>
+                                    <MenuItem id="radio" value="radio"><Radio style={{ margin: '0px', color: '#70757a' }} /> Multiple Choice</MenuItem>
                                 </Select>
                                 </div>
                                 {/* <IconButton>
@@ -81,12 +93,22 @@ const QuestionForm = ({editingMode}) => {
                                             <ShortText style={{ marginRight: '10px' }} />
                                         }
                                         <div>
-                                            <input type="text" className="text_input" placeholder="option" value={ques.options[j].optionText}></input>
+                                            <input type="text" className="text_input" placeholder="option" value={ques.options[j].optionText} defaultvalue={ques.options[j].optionText} onChange={(e) => {
+                                                ques.options[j].optionText = e.target.value;
+                                                const questionsAfterOptionChange = [...questions];
+                                                questions[i].options[j].optionText = e.target.value;
+                                                setQuestions(questionsAfterOptionChange);
+                                                }}></input>
                                         </div>
 
                                         <CropOriginal style={{ color: '#5f6368' }} />
 
-                                        <IconButton aria-label="delete">
+                                        <IconButton aria-label="delete" onClick={() => {
+                                            const index = j
+                                            let newquestions = [...questions];
+                                            newquestions[i].options.splice(index,1);
+                                            setQuestions(newquestions);
+                                        }}>
                                             <Close />
                                         </IconButton>
 
@@ -100,39 +122,44 @@ const QuestionForm = ({editingMode}) => {
                                         <QuestionAnswerIcon />
                                          Answer Key
                                     </Button>
-                                </div>
-                                :
-                                <div></div>
-                                }
-                                <div className="add_question_bottom">
-                                        <span style={{ color: '#5f6368', fontSize: '13px', fontFamily: 'Open Sans, Arial, sans-serif' }}>Required</span>
-                                        <FormControlLabel style={{margin: '0px'}} label="" control={<Switch color="primary" defaultChecked={ques.required} />} onChange={() => {ques.required = !ques.required }}/>
-                                        <IconButton onClick={() => deleteQuestion(ques.id)} aria-label="delete">
-                                            <DeleteIcon />
-                                        </IconButton>
-                                        <IconButton onClick={() => { createEmptyQuestion() }}>
+                                    <Tooltip title={`New ${ques.questionType} option`} arrow>
+                                        <IconButton onClick={() => addEmptyOption(i)}>
                                             <Add /> 
                                         </IconButton>
-                                        {/* <Switch label="checked" name="checkedA" color="primary" checked={ques.required}></Switch> */}
-                                    {/* <IconButton>
-                                        <MoreVert />
-                                    </IconButton>     */}
-
+                                    </Tooltip>
+                                </div>
+                                :
+                                <div>
+                                </div>
+                                }
+                                <div className="add_question_bottom" style={{borderTop: ques.options.length != 0 ? '1px solid #ccc' : ''}}>
+                                        <span style={{ color: '#5f6368', fontSize: '13px', fontFamily: 'Open Sans, Arial, sans-serif' }}>Required</span>
+                                        <FormControlLabel style={{margin: '0px'}} label="" control={<Switch color="primary" defaultChecked={ques.required} />} onChange={() => {ques.required = !ques.required }}/>
+                                        <Tooltip title="Delete question" arrow>
+                                            <IconButton onClick={() => deleteQuestion(ques.id)} aria-label="delete">
+                                                <DeleteIcon />
+                                            </IconButton>
+                                        </Tooltip>    
+                                        <Tooltip title="Add new question" arrow>
+                                            <IconButton onClick={() => { createEmptyQuestion() }}>
+                                                <PlaylistAdd />
+                                            </IconButton>
+                                        </Tooltip>
                                 </div>
                             </div>
                     </AccordionDetails>
                 </div>
             </Accordion>
         )) : questions.map((ques, i) => (
-            <Accordion key={i} expanded={ques.open} className={ques.open && 'add_bolder'}>
-                 <AccordionSummary aria-controls="panella-content" id="panella-header" elevation={1} style={{width: '100%'}}>
+            <Accordion sx={{background: 'white'}}  key={i} expanded={ques.open} className={ques.open && 'add_bolder'}>
+                 <AccordionSummary sx={{background: 'white'}} aria-controls="panella-content" id="panella-header" elevation={1} style={{width: '100%'}}>
                 {ques.open && (
                     <div className="saved_questions">
                         <Typography style={{ fontSize:'15px', fontWeight: '400', letterSpacing: '.1px', lineHeight: '24px',paddingBottom: '8px' }}>
                             {i+1}. {ques.questionText} {ques.required && <span style={{ color: 'red' }}>*</span>}
                         </Typography>
 
-                        {ques.questionType == 'text' && (<input type="text" /> )}
+                        {ques.questionType == 'text' && (<input style={{border: '1px solid #ccc', borderRadius: '5px', boxShadow: '5px 5px 5px rgba(5,5,5,.1)'}} className="text_input" type="text" name={`responseInputOfQuestion${i + 1}`} /> )}
 
                         {ques.options.map((option, j) =>(
                             <div key={j}>
